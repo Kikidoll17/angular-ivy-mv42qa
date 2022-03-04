67b8df2b8196bdcaedb188905759c3692550f5a2
Kikidoll17 <katierun97@gmail.com>
2022-03-04 11:52:33 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-mv42qa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-mv42qa)
---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Bob'
+  imageUrl:string = 'https://www.ndsu.edu/fileadmin/www.ur.ndsu.edu/logo_usage/NDSU.logo.typebox.jpg';
+
+  isSpecial:boolean = true; 
+
+  username:string = '';
+
+  onMouseOver():void{
+    this.isSpecial = !this.isSpecial;
+  }
+
+  onMouseOut(): void{
+    this.isSpecial = !this.isSpecial;
+  }
+
+  onClick():boolean{
+    return this.username == ' ';
+  }
+
+  
+}
